abdae5958a33c86e8d5c6e1a5c3862c6e9f223ad
Jake Pratte <japratte@gmail.com>
2020-10-16 17:32:54 -0400

    Add files via upload

---
+++ chessboardTrainer - (10-14) - og/script.js
@@ -0,0 +1,70 @@
+const numberCoords = ['1','2', '3', '4', '5', '6', '7', '8'];
+const letterCoords = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+const tiles = document.querySelectorAll('.tile');
+
+var ranNumCoord;
+var ranLetCoord;
+
+var ranCoord;
+
+tiles.forEach(tile => {
+    tile.addEventListener('click', () => showAnswer(tile))
+})
+
+
+
+
+function ranNumberGen(numberCoords) {
+    
+    var ranNum = numberCoords[Math.floor(Math.random() * numberCoords.length)]
+    
+    return ranNum;
+
+}
+
+function ranLetterGen(letterCoords) {
+
+    var ranLet = letterCoords[Math.floor(Math.random() * letterCoords.length)]
+
+    return ranLet;
+}
+
+function showCoordQuestion() {
+    
+    ranLetCoord = ranLetterGen(letterCoords);
+    ranNumCoord = ranNumberGen(numberCoords);
+    
+    ranCoord = ranLetCoord + ranNumCoord;
+
+    
+    document.getElementById("question").innerHTML = "Please click the " + ranCoord + " tile"
+
+}
+
+function showAnswer(tile) {
+    
+    if (tile.id != ranCoord) {
+        
+        document.getElementById("answer").innerHTML = "Incorrect - You clicked the " + tile.id + " tile !"
+    }
+    
+    if (tile.id == ranCoord) {
+        showCoordQuestion()
+        showCorrectAnswer()
+    }
+
+}
+
+function showCorrectAnswer() {
+    document.getElementById("answer").innerHTML = "Correct !"
+}
+
+function showCoords() {
+    
+    
+    
+    
+}
+
+showCoordQuestion();
